d7c0d19f3bb28288a5934ea9affaf73e04326ca5
arzugu <arzugul12@icloud.com>
2024-05-25 15:10:24 +0200

    commit to ReverseString

--- src/arzugul/week01/ReverseString.java
+++ src/arzugul/week01/ReverseString.java
@@ -4,6 +4,16 @@ public class ReverseString {
     public static void main(String[] args){
 
  String str= "Hello Team";
+ String reversedString ="";
+
+ for(int i=str.length()-1; i>=0; i--){
+     reversedString +=str.charAt(i);
+
+     System.out.println("Reversed String = " + reversedString);
+ }
+
+
+
 
 
 
